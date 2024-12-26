ced3cc8559c57ace8a4d216562acee45058742c2
user.name:"Rudra267 <nrudra550@gmail.com>
2024-12-26 10:46:41 +0530

    email Updates

--- js/email.js
+++ js/email.js
@@ -1,4 +1,3 @@
-const form = document.querySelector("form");
 const Fname = document.getElementById("Fname");
 const Lname = document.getElementById("Lname");
 const email = document.getElementById("email");
@@ -6,7 +5,7 @@ const phone = document.getElementById("PhoneNumber");
 const mess = document.getElementById("message");
 
 function sendEmail() {
-    const bodyMessage = `Full Name: ${Fname.value} ${Lname.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+    const bodyMessage = `Full Name: ${Fname.value} ${Lname.value} Email: ${email.value} Phone Number: ${phone.value} Message: ${mess.value}`;
     console.log(bodyMessage)
     var param = {
         to_name:"Stanza Soft Customer Service Team",
